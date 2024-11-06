43268fcf0dd6a1681535a60dd1e534b052bdbba1
Omar <Omar.roushdy@outlook.com>
2024-11-06 18:57:07 +0200

    Initialization and creation a blank project!

---
+++ src/minipaint/MiniPaint.java
@@ -0,0 +1,14 @@
+package minipaint;
+
+/**
+ * @author Omar
+ */
+
+public class MiniPaint {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    }
+    
+}
