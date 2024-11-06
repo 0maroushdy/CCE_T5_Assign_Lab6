9cf751f53d3756ed3fef4d950b496ec31d5d8c44
Omar <Omar.roushdy@outlook.com>
2024-11-06 22:48:19 +0200

    creating the main fram

--- src/minipaint/MiniPaint.java
+++ src/minipaint/MiniPaint.java
@@ -1,13 +1,17 @@
 package minipaint;
 
-/**
- * @author Omar
- */
+import Frontend.MainPage;
+import java.awt.Color;
 
 public class MiniPaint {
 
     public static void main(String[] args) {
         // TODO code application logic here
+        MainPage fr1 = new MainPage();
+        fr1.setLocationRelativeTo(null);
+        fr1.setVisible(true);
+        fr1.getContentPane().setBackground(new Color(240, 240,250));
+        fr1.setTitle("Vector Drawing Application");
         
     }
     
