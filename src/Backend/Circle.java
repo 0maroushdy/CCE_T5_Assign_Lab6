d832b89fb88ffa1069c053efa7436c2057b16c7d
Omar <Omar.roushdy@outlook.com>
2024-11-10 16:35:59 +0200

    refining the ShapeObject class

--- src/Backend/Circle.java
+++ src/Backend/Circle.java
@@ -1,4 +1,5 @@
 package Backend;
+import java.awt.Color;
 import java.awt.Point;
 import java.util.Map;
 
@@ -6,6 +7,11 @@ import java.util.Map;
  *
  * @author Omar
  */
-public class Circle extends ShapeObject implements Shape {
+public class Circle extends ShapeObject {
+    
+    public Circle(Point position, Map<String, Double> properties, Color color, Color fillColor) {
+        super(position, properties, color, fillColor);
+    }
+    
    
 }
--- src/Backend/ShapeObject.java
+++ src/Backend/ShapeObject.java
@@ -15,47 +15,56 @@ public abstract class ShapeObject implements Shape {
     private Color color;
     private Color fillColor;
     
-    // set a shape postion
+    // ------------------**Constractor**--------------------
+    public ShapeObject(Point position, Map<String, Double> properties, Color color, Color fillColor) {
+        this.position = position;
+        this.properties = properties;
+        this.color = color;
+        this.fillColor = fillColor;
+    }
+    
+    // ------------------**set shape postion**--------------------
     @Override
     public void setPosition(Point position){
-        
+        this.position.x = position.x;
+        this.position.y = position.y;
     }  
+
     @Override
-    public Point getPosition(){     
-       
+    public Point getPosition(){        
         return position;
     }
 
-    // update properties
+    // ------------------**update properties**--------------------
     @Override
     public void setProperties(Map<String, Double> properties){
-        
+        this.properties = properties;
     }
+    
     @Override
     public Map<String, Double> getProperties(){
         return properties;
     }
 
-    // colorize 
+    // ------------------**colorize**--------------------
     @Override
     public void setColor(Color color){
-        
+        this.color = color;
     }
     @Override
     public Color getColor(){
         return color;
     }
     @Override
-    public void setFillColor(Color color){
-        
+    public void setFillColor(Color fillColor){
+        this.fillColor = fillColor;
     }
     @Override
     public Color getFillColor(){
-        
         return fillColor;
     }
 
-    // redraw the shape on the canvas
+    // ------------------**redraw the shape on the canvas**--------------------
     @Override
     public void draw(java.awt.Graphics canvas){
         
